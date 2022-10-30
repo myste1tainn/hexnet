057c68bc9d8ce37cc8f4444d85cb33fafd1ec9e4
Arnon Keereena <arnon.keereena@accenture.com>
2022-10-30 11:39:03 +0700

    Conform `ErrorResponse` to go's `error`

--- Models.go
+++ Models.go
@@ -1,9 +1,15 @@
 package msnet
 
+import "fmt"
+
 type ErrorResponse struct {
 	Code           string      `json:"code"`
 	Message        string      `json:"message"`
 	Description    string      `json:"description"`
 	Title          string      `json:"title"`
 	AdditionalInfo interface{} `json:"additionalInfo"`
+}
+
+func (this *ErrorResponse) Error() string {
+	return fmt.Sprintf("%s: %s, %s, %s", this.Code, this.Title, this.Message, this.Description)
 }
